4c2c3bac94710a7b787af16124b9b0e39e5cbdd5
ozancinar <ozancinar86@yahoo.com>
2016-05-23 16:53:02 +0200

    adjusting Fisher's method with number effective tests.

--- R/fisher.r
+++ R/fisher.r
@@ -1,4 +1,12 @@
-fisher <- function(p, ...) {
-   k <- length(p)
-   pchisq(-2*sum(log(p)), df=2*k, lower.tail=FALSE)
+fisher <- function(p, adjust = "none", pca.method = NULL, R = NULL, ...) {
+   if(adjust == "none") {
+      k <- length(p)
+      pooled.p <- pchisq(-2*sum(log(p)), df=2*k, lower.tail=FALSE)
+   } else if(adjust == "m.eff") {
+      k <- length(p)
+      m.eff <- m.eff(p = p, R = R, method = pca.method)
+      pooled.p <- pchisq(-2 * sum(log(p))/ m.eff, df = 2 * m.eff, lower.tail = FALSE)
+   }
+   res <- list(p = pooled.p, adjust = paste0(adjust, " ", pca.method))
+   return(res)
 }
---
+++ R/m.eff.r
@@ -0,0 +1,46 @@
+
+m.eff <- function(p, R, method = NULL, ...) {
+  
+  # number of p-values
+  k <- length(p)
+  
+  # dimension checks
+  if(!isSymmetric(R)) {
+    stop("R is not symmetric.")
+  }
+  
+  if(nrow(R) != k) {
+    stop("Dimensions of R do not match with the length of p.")
+  }
+  
+  if(!method %in% c("nyholt", "li.ji", "gao", "galwey")) {
+     stop("The method for PCA is not correct.")
+  }
+  
+  # Converting the correlation matrix to positive-definite.
+  R <- as.matrix(nearPD(R)$mat)
+  
+  ### get eigenvalues and absolute eigenvalues of R matrix
+  evs <- eigen(R)$values
+  abs.evs <- abs(evs)
+  
+  if(method == "nyholt") {
+     ### effective number of tests (based on Nyholt, 2004)
+     m.eff <- 1 + (k - 1) * (1 - var(evs) / k)
+     m.eff <- floor(m.eff)
+  } else if(method == "li.ji") {
+     ### effective number of tests (based on Li & Ji, 2005)
+     m.eff <- sum(ifelse(abs.evs >= 1, 1, 0) + (abs.evs - floor(abs.evs)))
+     m.eff <- floor(m.eff)
+  } else if(method == "gao") {
+     ### effective number of tests (based on Gao, 2008)
+     m.eff <- which(cumsum(sort(abs.evs, decreasing = TRUE)) / sum(abs.evs) > 0.995)[1]
+  } else if(method == "galwey") {
+     ### effective number of tests (based on Galwey, 2009)
+     evs[evs < 0] <- 0
+     m.eff <- sum(sqrt(evs))^2 / sum(evs)
+     m.eff <- floor(m.eff)
+  }
+  
+  return(m.eff)
+}
