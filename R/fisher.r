a37720be379fb3670b6328f0f2044e953d1196d0
ozancinar <ozancinar86@yahoo.com>
2016-06-14 11:53:03 +0200

    adding Brown's method as an adjustment to fisher().

--- R/fisher.r
+++ R/fisher.r
@@ -6,7 +6,10 @@ fisher <- function(p, adjust = "none", pca.method = NULL, R = NULL, ...) {
       k <- length(p)
       eff <- meff(R = R, method = pca.method)
       pooled.p <- pchisq(-2 * sum(log(p))/ eff, df = 2 * eff, lower.tail = FALSE)
+   } else if (adjust == "brown") {
+      pooled.p <- brown(p, R)
    }
+   
    res <- list(p = pooled.p, adjust = paste0(adjust, " ", pca.method))
    return(res)
 }
--- R/minp.r
+++ R/minp.r
@@ -1,5 +1,5 @@
 
-minp <- function(p, adjust = "none") {
+minp <- function(p, adjust = "none", ...) {
    pooled.p <- min(p)
    res <- list(p = pooled.p, adjust = paste0(adjust, " "))
    return(res)
