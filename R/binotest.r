89c8a43fb38df5e65c35b3aa95d2c1c0c22e1497
ozancinar <ozancinar86@yahoo.com>
2016-06-17 16:29:50 +0200

    changes in binomial test and minimum p-value method.

--- R/binotest.r
+++ R/binotest.r
@@ -1,17 +1,17 @@
 
-binotest <- function(p, adjust = "none", pca.method = NULL, R = NULL, ...) {
+binotest <- function(p, adjust = "none", pca.method = NULL, R = NULL, alpha = 0.05, ...) {
    if(adjust == "none") {
       k <- length(p)
-      m <- sum(p <= 0.05)
-      probs <- dbinom(0:k, k, 0.05)
-      prob <- dbinom(m, k, 0.05)
+      m <- sum(p <= alpha)
+      probs <- dbinom(0:k, k, alpha)
+      prob <- dbinom(m, k, alpha)
       pooled.p <- sum(probs[probs <= prob])
    } else if(adjust == "m.eff") {
       k <- length(p)
-      m <- sum(p <= 0.05)
+      m <- sum(p <= alpha)
       eff <- meff(R = R, method = pca.method)
-      probs <- dbinom(0:eff, eff, 0.05)
-      prob <- dbinom(round(m * eff / k), eff, 0.05)
+      probs <- dbinom(0:eff, eff, alpha)
+      prob <- dbinom(round(m * eff / k), eff, alpha)
       pooled.p <- sum(probs[probs <= prob])
    }
    res <- list(p = pooled.p, adjust = paste0(adjust, " ", pca.method))
--- R/minp.r
+++ R/minp.r
@@ -1,6 +1,12 @@
 
-minp <- function(p, adjust = "none", ...) {
-   pooled.p <- min(p)
+minp <- function(p, adjust = "none", pca.method = NULL, R = NULL, ...) {
+   if (adjust == "none") {
+      k <- length(p)
+      pooled.p <- min(p) * k
+   } else if (adjust == "m.eff") {
+      eff <- meff(R = R, method = pca.method)
+      pooled.p <- min(p) * eff
+   }
    res <- list(p = pooled.p, adjust = paste0(adjust, " "))
    return(res)
 }
