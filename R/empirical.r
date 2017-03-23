5e33c7bb3560df0f3a3d33af3f89db08b3fd70bb
ozancinar <ozancinar86@yahoo.com>
2017-03-23 13:29:23 +0100

    modifying the empirical distributions for different types of hypothesis tests.

--- R/empirical.r
+++ R/empirical.r
@@ -1,5 +1,9 @@
 
-empirical <- function(p, R = NULL, method, size = 10000, seed = NULL, ...) {
+empirical <- function(p, R = NULL, method, type, size = 10000, seed = NULL, ...) {
+   
+   if(!type %in% c(-1, 1, 2)) {
+      stop("the type of the tests entered is not valid.")
+   }
    
    k <- length(p)
    
@@ -16,31 +20,61 @@ empirical <- function(p, R = NULL, method, size = 10000, seed = NULL, ...) {
    if(method == "bonferroni") {
       
       z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
-      pVals <- pnorm(z, lower.tail = FALSE)
+      if (type == -1) {
+         pVals <- pnorm(z, lower.tail = TRUE)
+      } else if (type == 1) {
+         pVals <- pnorm(z, lower.tail = FALSE)
+      } else if (type == 2) {
+         pVals <- 2 * pnorm(abs(z), lower.tail = TRUE)
+      }
       emp <- apply(pVals, 1, function(x) {bonferroni(x)$testStat})
    
    } else if(method == "tippett") {
       
       z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
-      pVals <- pnorm(z, lower.tail = FALSE)
+      if (type == -1) {
+         pVals <- pnorm(z, lower.tail = TRUE)
+      } else if (type == 1) {
+         pVals <- pnorm(z, lower.tail = FALSE)
+      } else if (type == 2) {
+         pVals <- 2 * pnorm(abs(z), lower.tail = TRUE)
+      }
       emp <- apply(pVals, 1, function(x) {tippett(x)$testStat})
          
    } else if(method == "binotest") {
       
       z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
-      pVals <- pnorm(z, lower.tail = FALSE)
+      if (type == -1) {
+         pVals <- pnorm(z, lower.tail = TRUE)
+      } else if (type == 1) {
+         pVals <- pnorm(z, lower.tail = FALSE)
+      } else if (type == 2) {
+         pVals <- 2 * pnorm(abs(z), lower.tail = TRUE)
+      }
       emp <- apply(pVals, 1, function(x) {binotest(x)$testStat})
       
    } else if(method == "fisher") {
       
       z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
-      pVals <- pnorm(z, lower.tail = FALSE)
+      if (type == -1) {
+         pVals <- pnorm(z, lower.tail = TRUE)
+      } else if (type == 1) {
+         pVals <- pnorm(z, lower.tail = FALSE)
+      } else if (type == 2) {
+         pVals <- 2 * pnorm(abs(z), lower.tail = TRUE)
+      }
       emp <- apply(pVals, 1, function(x) {fisher(x)$testStat})
       
    } else if(method == "stouffer") {
       
       z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
-      pVals <- pnorm(z, lower.tail = FALSE)
+      if (type == -1) {
+         pVals <- pnorm(z, lower.tail = TRUE)
+      } else if (type == 1) {
+         pVals <- pnorm(z, lower.tail = FALSE)
+      } else if (type == 2) {
+         pVals <- 2 * pnorm(abs(z), lower.tail = TRUE)
+      }
       emp <- apply(pVals, 1, function(x) {stouffer(x)$testStat})
    }
    
