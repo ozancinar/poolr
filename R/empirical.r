34a5c33c256fd0de3897c755428137e6f857617a
Wolfgang Viechtbauer <wvb@wvbauer.com>
2018-07-13 13:59:21 +0200

    In empirical(), use more consistent object naming.

--- R/empirical.r
+++ R/empirical.r
@@ -1,4 +1,4 @@
-empirical <- function(R, method, type, size = 10000, seed, ...) {
+emp.distirical <- function(R, method, type, size = 10000, seed, ...) {
 
    # match method argument
    method <- match.arg(method, c("fisher", "stouffer", "invchisq", "binotest", "bonferroni", "tippett"))
@@ -19,32 +19,31 @@ empirical <- function(R, method, type, size = 10000, seed, ...) {
    if (!missing(seed))
       set.seed(seed)
 
-   z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
+   Z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
 
-   if (type == 1) {
-      pVals <- pnorm(z, lower.tail = FALSE)
-   } else if (type == 2) {
-      pVals <- 2 * pnorm(abs(z), lower.tail = FALSE)
-   }
+   if (type == 1)
+      P <- pnorm(Z, lower.tail = FALSE)
+   if (type == 2)
+      P <- 2 * pnorm(abs(Z), lower.tail = FALSE)
 
    if (method == "fisher")
-      emp <- apply(pVals, 1, function(x) fisher(x)$testStat)
+      emp.dist <- apply(P, 1, function(x) fisher(x)$testStat)
 
    if (method == "stouffer")
-      emp <- apply(pVals, 1, function(x) stouffer(x)$testStat)
+      emp.dist <- apply(P, 1, function(x) stouffer(x)$testStat)
 
    if (method == "invchisq")
-      emp <- apply(pVals, 1, function(x) invchisq(x)$testStat)
+      emp.dist <- apply(P, 1, function(x) invchisq(x)$testStat)
 
    if (method == "binotest")
-      emp <- apply(pVals, 1, function(x) binotest(x)$testStat)
+      emp.dist <- apply(P, 1, function(x) binotest(x)$testStat)
 
    if (method == "bonferroni")
-      emp <- apply(pVals, 1, function(x) bonferroni(x)$testStat)
+      emp.dist <- apply(P, 1, function(x) bonferroni(x)$testStat)
 
    if (method == "tippett")
-      emp <- apply(pVals, 1, function(x) tippett(x)$testStat)
+      emp.dist <- apply(P, 1, function(x) tippett(x)$testStat)
 
-   return(emp)
+   return(emp.dist)
 
 }
