6110daa46ebe013ec03abeed2aaeb0afa40a0497
ozancinar <ozancinar86@gmail.com>
2018-07-20 05:30:33 +0200

    adding a warning if empirical used nearPD().

--- R/empirical.r
+++ R/empirical.r
@@ -1,49 +1,56 @@
 empirical <- function(R, method, type, size = 10000, seed, ...) {
+  
+  # match method argument
+  method <- match.arg(method, c("fisher", "stouffer", "invchisq", "binotest", 
+                                "bonferroni", "tippett"))
 
-   # match method argument
-   method <- match.arg(method, c("fisher", "stouffer", "invchisq", "binotest", "bonferroni", "tippett"))
+  # check type argument
+  if (!type %in% c(1, 2))
+      stop("argument 'type' must be either 1 or 2.")
 
-   # check type argument
-   if (!type %in% c(1, 2))
-      stop("Argument 'type' must be either 1 or 2.")
+  k <- nrow(R)
 
-   k <- nrow(R)
-
-   # check that R is symmetric
-   if (!isSymmetric(R))
+  # check that R is symmetric
+  if (!isSymmetric(R))
       stop("R is not symmetric.")
 
-   # ensure that the correlation matrix is positive semi-definite
-   R <- nearPD(R)$mat
+  # checking if the correlation matrix is positive definite by testing if all 
+  # eigen-values are positive.
+  if(!all(eigen(R)$values > 0)) {
+    
+    R <- nearPD(R)$mat
+    warning("the correlation matrix is not positive definite. empirical() used Matrix::nearPD to make the correlation matrix positive definite.")
+    
+  }
+  
+  if (!missing(seed))
+    set.seed(seed)
 
-   if (!missing(seed))
-      set.seed(seed)
-
-   Z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
-
-   if (type == 1)
-      P <- pnorm(Z, lower.tail = FALSE)
-   if (type == 2)
-      P <- 2 * pnorm(abs(Z), lower.tail = FALSE)
-
-   if (method == "fisher")
-      emp.dist <- apply(P, 1, function(x) fisher(x)$testStat)
-
-   if (method == "stouffer")
-      emp.dist <- apply(P, 1, function(x) stouffer(x)$testStat)
-
-   if (method == "invchisq")
-      emp.dist <- apply(P, 1, function(x) invchisq(x)$testStat)
-
-   if (method == "binotest")
-      emp.dist <- apply(P, 1, function(x) binotest(x)$testStat)
-
-   if (method == "bonferroni")
-      emp.dist <- apply(P, 1, function(x) bonferroni(x)$testStat)
-
-   if (method == "tippett")
-      emp.dist <- apply(P, 1, function(x) tippett(x)$testStat)
-
-   return(emp.dist)
-
+  Z <- mvrnorm(size, mu = rep(0, k), Sigma = R)
+  
+  if (type == 1)
+    P <- pnorm(Z, lower.tail = FALSE)
+  if (type == 2)
+    P <- 2 * pnorm(abs(Z), lower.tail = FALSE)
+  
+  if (method == "fisher")
+    emp.dist <- apply(P, 1, function(x) fisher(x)$testStat)
+  
+  if (method == "stouffer")
+    emp.dist <- apply(P, 1, function(x) stouffer(x)$testStat)
+  
+  if (method == "invchisq")
+    emp.dist <- apply(P, 1, function(x) invchisq(x)$testStat)
+  
+  if (method == "binotest")
+    emp.dist <- apply(P, 1, function(x) binotest(x)$testStat)
+  
+  if (method == "bonferroni")
+    emp.dist <- apply(P, 1, function(x) bonferroni(x)$testStat)
+  
+  if (method == "tippett")
+    emp.dist <- apply(P, 1, function(x) tippett(x)$testStat)
+  
+  return(emp.dist)
+  
 }
