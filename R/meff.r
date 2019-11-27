dbbe1614f3db3843cd0276f0dd6044f0074bff64
Wolfgang Viechtbauer <wvb@wvbauer.com>
2019-11-27 09:46:02 +0100

    Allow specification of value for C for method=gao via ... in meff().

--- R/meff.r
+++ R/meff.r
@@ -3,6 +3,8 @@ meff <- function(R, eigen, method, ...) {
    # match 'method' argument
    method <- match.arg(method, c("nyholt", "liji", "gao", "galwey"))
 
+   ddd <- list(...)
+
    if (missing(eigen)) {
 
       # check if 'R' is specified
@@ -31,27 +33,43 @@ meff <- function(R, eigen, method, ...) {
    }
 
    if (method == "nyholt") {
+
       # effective number of tests (based on Nyholt, 2004)
       k <- length(evs)
       m <- 1 + (k - 1) * (1 - var(evs) / k)
+
    }
 
    if (method == "liji") {
+
       # effective number of tests (based on Li & Ji, 2005)
       # adding a small value to the absolute eigenvalues to overcome numerical imprecisions
       abs.evs <- abs(evs) + sqrt(.Machine$double.eps)
       m <- sum(ifelse(abs.evs >= 1, 1, 0) + (abs.evs - floor(abs.evs)))
+
    }
 
    if (method == "gao") {
+
       # effective number of tests (based on Gao, 2008)
-      m <- which(cumsum(sort(evs, decreasing = TRUE)) / sum(evs) > 0.995)[1]
+
+      # allow user to specify value of C via ... but otherwise use 0.995
+      if (!is.null(ddd$C)) {
+         C <- ddd$C
+      } else {
+         C <- 0.995
+      }
+
+      m <- which(cumsum(sort(evs, decreasing = TRUE)) / sum(evs) > C)[1]
+
    }
 
    if (method == "galwey") {
+
       # effective number of tests (based on Galwey, 2009)
       evs[evs < 0] <- 0
       m <- sum(sqrt(evs))^2 / sum(evs)
+
    }
 
    # always round down the estimated value
